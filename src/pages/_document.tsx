53440a76cab0f1a9c732ef9cbf5309c397b83594
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-08-22 08:54:28 +0700

    fix seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="!scroll-smooth">
-      <Head />
+      <Head>
+        <meta
+          name="M Dhiya Ulhaq"
+          content="Biography Muhammad Dhiya Ulhaq"
+          key="Frontend Develper"
+        />
+      </Head>
       <body>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[32.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[32.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
